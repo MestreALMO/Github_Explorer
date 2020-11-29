ffec316fa04121457a2d4454a9921208da376bd6
MestreALMO <lusegardis@gmail.com>
2020-11-29 20:16:45 -0300

    Fixing erros, conecting with API, updating Dashboard, style-component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,86 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+  /** Nem tudo que retorna precisa ser colocado na tipagem, cria a tipagem só do que será utilizado */
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  // eslint-disable-next-line prettier/prettier
+  const [newRepo, setNewRepo] = useState(''); /** Estado só pra armazenar valor do input */
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Digite "autor/nome_do repositório"');
+      return;
+    }
+
+    try {
+      const response = await api.get(`repos/${newRepo}`);
+      /** Procurando por "rocketseat/unform" vai retorna a DB do site no console.log */
+
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro na busca por esse repositório');
+    }
+  }
+
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositorys in Github</Title>
+
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          // eslint-disable-next-line prettier/prettier
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digit name of repository"
+        />
+        <button type="submit">Research</button>
+      </Form>
+
+      {inputError && <Error>{inputError}</Error>}
+
+      <Repositories>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
